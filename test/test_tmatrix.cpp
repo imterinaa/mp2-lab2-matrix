333614cbc02f01ab78e3bb1e1adf149e8a9177be
alinaterina <alinaterina@mail.ru>
2021-10-13 22:44:37 +0300

    13-10

--- test/test_tmatrix.cpp
+++ test/test_tmatrix.cpp
@@ -4,108 +4,206 @@
 
 TEST(TMatrix, can_create_matrix_with_positive_length)
 {
-  ASSERT_NO_THROW(TMatrix<int> m(5));
+ASSERT_NO_THROW(TMatrix<int> m(5));
 }
 
 TEST(TMatrix, cant_create_too_large_matrix)
 {
-  ASSERT_ANY_THROW(TMatrix<int> m(MAX_MATRIX_SIZE + 1));
+ASSERT_ANY_THROW(TMatrix<int> m(MAX_MATRIX_SIZE + 1));
 }
 
 TEST(TMatrix, throws_when_create_matrix_with_negative_length)
 {
-  ASSERT_ANY_THROW(TMatrix<int> m(-5));
+ASSERT_ANY_THROW(TMatrix<int> m(-5));
 }
 
 TEST(TMatrix, can_create_copied_matrix)
 {
-  TMatrix<int> m(5);
+TMatrix<int> m(5);
 
-  ASSERT_NO_THROW(TMatrix<int> m1(m));
+ASSERT_NO_THROW(TMatrix<int> m1(m));
 }
 
 TEST(TMatrix, copied_matrix_is_equal_to_source_one)
 {
-  ADD_FAILURE();
+TMatrix<int> m1(5), m2(m1);
+
+EXPECT_EQ(m1, m2);
 }
 
 TEST(TMatrix, copied_matrix_has_its_own_memory)
 {
-  ADD_FAILURE();
+TMatrix<int> m1(3);
+
+m1[0][0] = 0;
+
+TMatrix<int> m2(m1);
+
+m1[0][0] = 1;
+
+EXPECT_EQ(0, m2[0][0]);
 }
 
 TEST(TMatrix, can_get_size)
 {
-  ADD_FAILURE();
+const int SIZE = 5;
+
+TMatrix<int> m(SIZE);
+
+EXPECT_EQ(SIZE, m.GetSize());
 }
 
 TEST(TMatrix, can_set_and_get_element)
 {
-  ADD_FAILURE();
+TMatrix<int> m(2);
+m[0][0] = 4;
+
+EXPECT_EQ(4, m[0][0]);
 }
 
 TEST(TMatrix, throws_when_set_element_with_negative_index)
 {
-  ADD_FAILURE();
+TMatrix<int> m(2);
+
+ASSERT_ANY_THROW(m[-1][0] = 0);
 }
 
 TEST(TMatrix, throws_when_set_element_with_too_large_index)
 {
-  ADD_FAILURE();
+TMatrix<int> m(4);
+
+ASSERT_ANY_THROW(m[4][0] = 0);
 }
 
 TEST(TMatrix, can_assign_matrix_to_itself)
 {
-  ADD_FAILURE();
+TMatrix<int> m(3);
+
+ASSERT_NO_THROW(m = m);
 }
 
 TEST(TMatrix, can_assign_matrices_of_equal_size)
 {
-  ADD_FAILURE();
+const int SIZE = 5;
+
+TMatrix<int> m1(SIZE), m2(SIZE);
+
+m2 = m1;
+
+EXPECT_EQ(m1, m2);
 }
 
 TEST(TMatrix, assign_operator_change_matrix_size)
 {
-  ADD_FAILURE();
+const int SIZE1 = 5, SIZE2 = 3;
+
+TMatrix<int> m1(SIZE1), m2(SIZE2);
+
+m2 = m1;
+
+EXPECT_EQ(5, m2.GetSize());
 }
 
 TEST(TMatrix, can_assign_matrices_of_different_size)
 {
-  ADD_FAILURE();
+const int SIZE1 = 5, SIZE2 = 3;
+
+TMatrix<int> m1(SIZE1), m2(SIZE2);
+
+m2 = m1;
+
+EXPECT_EQ(m1, m2);
 }
 
 TEST(TMatrix, compare_equal_matrices_return_true)
 {
-  ADD_FAILURE();
+const int SIZE = 5;
+
+TMatrix<int> m1(SIZE), m2(SIZE);
+
+for (int i = 0; i < SIZE; i++)
+for (int j = i; j < SIZE; j++)
+m1[i][j] = 1;
+
+m2 = m1;
+
+EXPECT_EQ(1, m1 == m2);
 }
 
 TEST(TMatrix, compare_matrix_with_itself_return_true)
 {
-  ADD_FAILURE();
+const int SIZE = 5;
+
+TMatrix<int> m(SIZE);
+
+EXPECT_EQ(1, m == m);
 }
 
 TEST(TMatrix, matrices_with_different_size_are_not_equal)
 {
-  ADD_FAILURE();
+const int SIZE1 = 5, SIZE2 = 3;
+
+TMatrix<int> m1(SIZE1), m2(SIZE2);
+
+EXPECT_EQ(1, m1 != m2);
 }
 
 TEST(TMatrix, can_add_matrices_with_equal_size)
 {
-  ADD_FAILURE();
+const int SIZE = 5;
+
+TMatrix<int> m1(SIZE), m2(SIZE), m3(SIZE);
+
+for (int i = 0; i < SIZE; i++)
+for (int j = i; j < SIZE; j++)
+m1[i][j] = 1;
+
+for (int i = 0; i < SIZE; i++)
+for (int j = i; j < SIZE; j++)
+m2[i][j] = 2;
+
+for (int i = 0; i < SIZE; i++)
+for (int j = i; j < SIZE; j++)
+m3[i][j] = 3;
+
+EXPECT_EQ(m3, m1 + m2);
 }
 
 TEST(TMatrix, cant_add_matrices_with_not_equal_size)
 {
-  ADD_FAILURE();
+const int SIZE1 = 5, SIZE2 = 3;
+
+TMatrix<int> m1(SIZE1), m2(SIZE2);
+
+ASSERT_ANY_THROW(m1 + m2);
 }
 
 TEST(TMatrix, can_subtract_matrices_with_equal_size)
 {
-  ADD_FAILURE();
+const int SIZE = 5;
+
+TMatrix<int> m1(SIZE), m2(SIZE), m3(SIZE);
+
+for (int i = 0; i < SIZE; i++)
+for (int j = i; j < SIZE; j++)
+m1[i][j] = 1;
+
+for (int i = 0; i < SIZE; i++)
+for (int j = i; j < SIZE; j++)
+m2[i][j] = 2;
+
+for (int i = 0; i < SIZE; i++)
+for (int j = i; j < SIZE; j++)
+m3[i][j] = 3;
+
+EXPECT_EQ(m1, m3 - m2);
 }
 
 TEST(TMatrix, cant_subtract_matrixes_with_not_equal_size)
 {
-  ADD_FAILURE();
-}
+const int SIZE1 = 5, SIZE2 = 3;
 
+TMatrix<int> m1(SIZE1), m2(SIZE2);
+
+ASSERT_ANY_THROW(m1 - m2);
+}
